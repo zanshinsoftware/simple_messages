d9bd9a062cf21316ee21dbbe07cb0ba293832c4b
Felipe Diesel <felipediesel@gmail.com>
2015-05-28 17:10:39 -0300

    Updating tests.

--- spec/lib/simple_messages/builder_spec.rb
+++ spec/lib/simple_messages/builder_spec.rb
@@ -6,7 +6,7 @@ describe SimpleMessages::Builder do
       body: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
   end
 
-  it { expect(@builder.header_wrapper).to eq '<h4>This title my alert message.</h4>' }
+  it { expect(@builder.header_wrapper).to eq '<strong>This title my alert message.</strong><br>' }
 
   it { expect(@builder.close_wrapper).to eq "<button type='button' class='close' data-dismiss='alert'><span aria-hidden='true'>&times;</span><span class='sr-only'>Close</span></button>" }
 
@@ -14,7 +14,7 @@ describe SimpleMessages::Builder do
 
   it { expect(@builder.has_header?).to eq true }
 
-  it { expect(@builder.to_html).to eq "<div class='alert alert-success alert-dismissible' role='alert'><button type='button' class='close' data-dismiss='alert'><span aria-hidden='true'>&times;</span><span class='sr-only'>Close</span></button><h4>This title my alert message.</h4>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>" }
+  it { expect(@builder.to_html).to eq "<div class='alert alert-success alert-dismissible' role='alert'><button type='button' class='close' data-dismiss='alert'><span aria-hidden='true'>&times;</span><span class='sr-only'>Close</span></button><strong>This title my alert message.</strong><br>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>" }
 
   context "without close" do
     before do
@@ -23,7 +23,7 @@ describe SimpleMessages::Builder do
 
     it { expect(@builder.closable?).to eq false }
 
-    it { expect(@builder.to_html).to eq "<div class='alert alert-success' role='alert'><h4>This title my alert message.</h4>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>" }
+    it { expect(@builder.to_html).to eq "<div class='alert alert-success' role='alert'><strong>This title my alert message.</strong><br>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>" }
   end
 
   context "without header" do
@@ -43,7 +43,7 @@ describe SimpleMessages::Builder do
       @builder.body = [ 'Line 1.', 'Line 2.' ]
     end
 
-    it { expect(@builder.to_html).to eq "<div class='alert alert-success alert-dismissible' role='alert'><button type='button' class='close' data-dismiss='alert'><span aria-hidden='true'>&times;</span><span class='sr-only'>Close</span></button><h4>This title my alert message.</h4>Line 1.<br />Line 2.</div>" }
+    it { expect(@builder.to_html).to eq "<div class='alert alert-success alert-dismissible' role='alert'><button type='button' class='close' data-dismiss='alert'><span aria-hidden='true'>&times;</span><span class='sr-only'>Close</span></button><strong>This title my alert message.</strong><br>Line 1.<br />Line 2.</div>" }
   end
 
   context "notice kind" do
@@ -51,7 +51,7 @@ describe SimpleMessages::Builder do
       @builder.kind = :notice
     end
 
-    it { expect(@builder.to_html).to eq "<div class='alert alert-success alert-dismissible' role='alert'><button type='button' class='close' data-dismiss='alert'><span aria-hidden='true'>&times;</span><span class='sr-only'>Close</span></button><h4>This title my alert message.</h4>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>" }
+    it { expect(@builder.to_html).to eq "<div class='alert alert-success alert-dismissible' role='alert'><button type='button' class='close' data-dismiss='alert'><span aria-hidden='true'>&times;</span><span class='sr-only'>Close</span></button><strong>This title my alert message.</strong><br>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>" }
   end
 
   context "danger kind" do
@@ -59,7 +59,7 @@ describe SimpleMessages::Builder do
       @builder.kind = :danger
     end
 
-    it { expect(@builder.to_html).to eq "<div class='alert alert-danger alert-dismissible' role='alert'><button type='button' class='close' data-dismiss='alert'><span aria-hidden='true'>&times;</span><span class='sr-only'>Close</span></button><h4>This title my alert message.</h4>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>" }
+    it { expect(@builder.to_html).to eq "<div class='alert alert-danger alert-dismissible' role='alert'><button type='button' class='close' data-dismiss='alert'><span aria-hidden='true'>&times;</span><span class='sr-only'>Close</span></button><strong>This title my alert message.</strong><br>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>" }
   end
 
   context "alert kind" do
@@ -67,7 +67,7 @@ describe SimpleMessages::Builder do
       @builder.kind = :alert
     end
 
-    it { expect(@builder.to_html).to eq "<div class='alert alert-warning alert-dismissible' role='alert'><button type='button' class='close' data-dismiss='alert'><span aria-hidden='true'>&times;</span><span class='sr-only'>Close</span></button><h4>This title my alert message.</h4>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>" }
+    it { expect(@builder.to_html).to eq "<div class='alert alert-warning alert-dismissible' role='alert'><button type='button' class='close' data-dismiss='alert'><span aria-hidden='true'>&times;</span><span class='sr-only'>Close</span></button><strong>This title my alert message.</strong><br>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>" }
   end
 
   context "with html attributes" do
@@ -75,6 +75,6 @@ describe SimpleMessages::Builder do
       @builder.html = { class: 'my-class', id: 'my-id' }
     end
 
-    it { expect(@builder.to_html).to eq "<div class='my-class alert alert-success alert-dismissible' role='alert' id='my-id'><button type='button' class='close' data-dismiss='alert'><span aria-hidden='true'>&times;</span><span class='sr-only'>Close</span></button><h4>This title my alert message.</h4>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>" }
+    it { expect(@builder.to_html).to eq "<div class='my-class alert alert-success alert-dismissible' role='alert' id='my-id'><button type='button' class='close' data-dismiss='alert'><span aria-hidden='true'>&times;</span><span class='sr-only'>Close</span></button><strong>This title my alert message.</strong><br>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</div>" }
   end
 end
